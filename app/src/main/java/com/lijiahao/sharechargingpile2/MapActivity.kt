f9f8f0a570f61de5f30779ae0c2eb6259d918dcc
李嘉浩 <1040374467@qq.com>
2022-03-06 20:12:53 +0800

    add dependencies & configure the build script

---
+++ app/src/main/java/com/lijiahao/sharechargingpile2/MapActivity.kt
@@ -0,0 +1,11 @@
+package com.lijiahao.sharechargingpile2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MapActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_map)
+    }
+}
